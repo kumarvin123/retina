f3571f666dd5f91dc6c9a56087a75649ca743047
Venkata Naga Kartik Pidatala <vpidatala@microsoft.com>
2025-03-04 10:13:35 -0800

    event-writer crash detect#38

--- test/e2e/tools/event-writer/bpf_event_writer.c
+++ test/e2e/tools/event-writer/bpf_event_writer.c
@@ -83,17 +83,15 @@ void create_trace_ntfy_event(struct trace_notify* trc_elm)
     trc_elm->subtype    = 0;
 	trc_elm->source     = 0;
 	trc_elm->hash       = 0;
-    /*
     trc_elm->len_orig   = 128;
 	trc_elm->len_cap    = 128;
-	trc_elm->version    = 1;
+    trc_elm->version    = 1;
 	trc_elm->src_label	= 0;
 	trc_elm->dst_label	= 0;
 	trc_elm->dst_id		= 0;
 	trc_elm->reason		= 0;
 	trc_elm->ifindex	= 0;
 	trc_elm->ipv6		= 0;
-    */
 }
 
 void create_drop_event(struct drop_notify* drp_elm)
