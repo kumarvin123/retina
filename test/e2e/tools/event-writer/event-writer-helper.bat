b1c90f090d40fc2cfcddc78a91e0f9435cf6e36a
Venkata Naga Kartik Pidatala <vpidatala@microsoft.com>
2025-03-04 18:46:22 -0800

    event-writer winbpf

---
+++ test/e2e/tools/event-writer/event-writer-helper.bat
@@ -0,0 +1,35 @@
+@echo on
+REM Define the Setup-EventWriter function
+:Setup-EventWriter
+   echo Listing contents of C:\
+   dir C:\
+
+   echo Copying event_writer.exe to C:\
+   copy .\event_writer.exe C:\event_writer.exe
+
+   echo Copying bpf_event_writer.sys to C:\
+   copy .\bpf_event_writer.sys C:\bpf_event_writer.sys
+
+   echo Changing directory to C:\ and starting event_writer.exe
+   goto :EOF
+
+REM Define the Start-EventWriter function
+:Start-EventWriter
+   echo Changing directory to C:\
+   cd C:\
+
+   echo Starting event_writer.exe with -event 4
+   start .\event_writer.exe -event 4
+
+   echo Changing directory to C:\hpc
+   cd C:\hpc
+
+   goto :EOF
+
+REM Define the GetPromMetrics function
+:GetRetinaPromMetrics
+   echo Fetching Prometheus metrics from http://localhost:10093/metrics
+   powershell -Command "Invoke-WebRequest -Uri 'http://localhost:10093/metrics' -UseBasicParsing | ForEach-Object { $_.Content }"
+
+   goto :EOF
+
