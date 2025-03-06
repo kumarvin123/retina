c1db8aac180a00c5aea03fc812b8f8f4f90eeaa4
Venkata Naga Kartik Pidatala <vpidatala@microsoft.com>
2025-03-05 19:38:45 -0800

    event-writer winbpf#24

--- test/e2e/tools/event-writer/event-writer-helper.bat
+++ test/e2e/tools/event-writer/event-writer-helper.bat
@@ -9,33 +9,37 @@ goto :EOF
 
 REM Define the Setup-EventWriter function
 :Setup-EventWriter
-   powershell -Command "Write-Output 'Listing contents of C:\'"
+   powershell -Command "Write-Output '                        '"
+   echo "Listing contents of C:"
+
    dir C:\
 
-   powershell -Command "Write-Output 'Copying event_writer.exe to C:'"
+   echo "Copying event_writer.exe to C:"
    copy .\event_writer.exe C:\event_writer.exe
 
-    powershell -Command "Write-Output 'Copying bpf_event_writer.sys to C:'"
+   echo "Copying bpf_event_writer.sys to C:"
    copy .\bpf_event_writer.sys C:\bpf_event_writer.sys
 
-   powershell -Command "Write-Output 'Listing contents of C:'"
+   echo "Listing contents of C:"
    dir C:\
 
    goto :EOF
 
 REM Define the Start-EventWriter function
 :Start-EventWriter
+   powershell -Command "Write-Output '                        '"
+
    echo Changing directory to C:\
    cd C:\
    echo Starting event_writer.exe with -event %3 -srcIP %5
    .\event_writer.exe -event %3 -srcIP %5
    echo Changing directory to C:\hpc
-   cd C:\hpc
 
    goto :EOF
 
 REM Define the GetPromMetrics function
 :GetRetinaPromMetrics
+   powershell -Command "Write-Output '                        '"
    echo Fetching Prometheus metrics from http://localhost:10093/metrics
    powershell -Command "Invoke-WebRequest -Uri 'http://localhost:10093/metrics' -UseBasicParsing | ForEach-Object { $_.Content }"
 
@@ -43,7 +47,8 @@ REM Define the GetPromMetrics function
 
 REM Curl AKA.MS
 :CurlAkaMs
-   // Hardcoding IP addr for aka.ms - 23.213.38.151
+   powershell -Command "Write-Output '                        '"
+   REM Hardcoding IP addr for aka.ms - 23.213.38.151
    echo Curl AKA.MS or 23.213.38.151
    start /B cmd /c "for /L %%i in (1,1,1000) do (powershell -Command \"Invoke-WebRequest -Uri 'http://23.213.38.151' -UseBasicParsing\" & timeout /t 1 >nul)"
    goto :EOF
