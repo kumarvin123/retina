d545deb0308ddb70149237eee12d331b6216fe8e
Venkata Naga Kartik Pidatala <vpidatala@microsoft.com>
2025-03-13 00:29:30 -0700

    all metrics#4

--- test/e2e/tools/event-writer/bpf_event_writer.c
+++ test/e2e/tools/event-writer/bpf_event_writer.c
@@ -239,8 +239,7 @@ event_writer(xdp_md_t* ctx) {
         if (drp_elm == NULL) {
             return XDP_PASS;
         }
-        // Drop Reason to Drop_Filtered,
-        reason = 8;
+        reason = 6;
         create_drop_event(drp_elm);
         memset(drp_elm->data, 0, sizeof(drp_elm->data));
         memcpy(drp_elm->data, ctx->data, size_to_copy);
